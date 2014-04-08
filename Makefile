d0f556ca76173c77b46372539a1dc830c31e5d0f
jplehr <jp.lehr@gmail.com>
2014-04-08 12:11:08 +0000

    JP: Check in my cube lib playground.
    git-svn-id: https://svn.rwth-aachen.de/repos/ci841521_Instrumentor/jplehr/CubeCallGraph@832 9bac9e94-af68-4b6e-8904-753e07678547

--- Makefile
+++ Makefile
@@ -5,8 +5,12 @@ INCLUDEFLAGS=-I. -I/home/ci24amun/myRoot/gcc/openmpi/cube/4.2.1/include/cube/
 
 LDFLAGS+=-L/home/ci24amun/myRoot/gcc/openmpi/cube/4.2.1/lib -lcube4 -lz
 
-CubeCallGraphTool:
+CubeCallGraphTool-lcluster:
 	$(CXX) $(CXXFLAGS) $(INCLUDEFLAGS) -o CubeCallgraphTool.exe main.cpp cgNode.cpp callgraph.cpp $(LDFLAGS)
+
+
+CubeOverheadEstimater-mice:
+	$(CXX) $(CXXFLAGS) -I /home/jpatrick/libs/cube/install-gcc-4.8.2/include/cube -o CubeOverheadEstimation main.cpp -L/home/jpatrick/libs/cube/install-gcc-4.8.2/lib -lcube4 -lz
 
 clean:
 	rm -rf *.o
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include "callgraph.h"
 #include "Cube.h"
+#include "CubeMetric.h"
 #include <cstdlib>
 
 #include <cassert>
@@ -8,7 +9,7 @@
 int main(int argc, char** argv){
 
 
-Callgraph cg;
+//Callgraph cg;
 // The cube documentation says one should always use a try/catch thing around
 try{
 	// Create cube instance
@@ -20,6 +21,7 @@ try{
 
 	const std::vector<cube::Cnode*>& cnodes = cube.get_cnodev();
 
+#if 0
 	std::cout << cnodes[0]->get_callee()->get_name() << std::endl;
 //	std::cout << cnodes[0]->get_callee()->get_mod() << std::endl;
 
@@ -36,8 +38,31 @@ try{
 
 	for(auto node : cg.getNodesToMark())
 		node->printMinimal();
+#endif
 
-	
+	/** JP: This is code to estimate the generated overhead via call-graph guided hook placement. */
+	bool once = true;
+	std::vector<double> calls;
+	cube::Metric* metric = cube.get_met( "visits" );
+	const std::vector<cube::Thread*> threads = cube.get_thrdv();
+	for( auto node : cnodes){
+		std::cout << node->get_mod() << " : " << node->get_callee()->get_name() << " : " << cube.get_sev(metric, node, threads.at(0)) << std::endl;
+	}
+#if 0
+//		calls.push_back(cube.get_sev(metric, node, threads.at(0)));
+		if(!once){
+			const std::vector<cube::Metric*> metrics = cube.get_metv();
+			for(auto m : metrics){
+				std::cout << "Metric unique name: " << m->get_uniq_name() << " Metric display name: " << m->get_disp_name() << std::endl;
+			}
+			once = true;
+		}
+	}
+
+	for(auto d : calls){
+		std::cout << d << std::endl;
+	}
+#endif
 
 } catch(cube::RuntimeError e){
 	std::cout << e.get_msg() << std::endl;
