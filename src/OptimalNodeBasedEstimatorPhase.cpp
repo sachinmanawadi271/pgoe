45c367320d579d7211d8cb2251d8c7c79df28373
roman <kaossilat0r.rn@gmail.com>
2014-07-22 14:40:51 +0000

    .. and forgot the new files
    git-svn-id: https://svn.rwth-aachen.de/repos/ci841521_Instrumentor/rness/branches/CubeCallGraph@998 9bac9e94-af68-4b6e-8904-753e07678547

---
+++ src/OptimalNodeBasedEstimatorPhase.cpp
@@ -0,0 +1,63 @@
+
+#include "OptimalNodeBasedEstimatorPhase.h"
+
+OptimalNodeBasedEstimatorPhase::OptimalNodeBasedEstimatorPhase() :
+		EstimatorPhase("OptimalNodeBased"),
+		optimalCosts(INT64_MAX){
+}
+
+OptimalNodeBasedEstimatorPhase::~OptimalNodeBasedEstimatorPhase() {
+}
+
+void OptimalNodeBasedEstimatorPhase::modifyGraph(CgNodePtr mainMethod) {
+
+	findStartingState(mainMethod);
+
+	while (!stateStack.empty()) {
+
+		// XXX RN i'm not sure this will work as intended if the stack is modified
+		for (auto node : stateStack.top().currentInstrumentation) {
+			auto parentNodes = node->getParentNodes();
+
+			auto newState(stateStack.top());
+
+			if (newState.validAfterExchange(node, parentNodes)) {
+				// TODO: calculate costs
+
+				stateStack.push(newState);
+			}
+
+		}
+
+		stateStack.pop();	// XXX is this correct?
+	}
+
+}
+
+void OptimalNodeBasedEstimatorPhase::printAdditionalReport() {
+	// TODO additional report
+}
+
+/** fill the initial stack with all parents of conjunctions and all initial constraints */
+void OptimalNodeBasedEstimatorPhase::findStartingState(CgNodePtr mainMethod) {
+
+	CgNodePtrSet startingParents = {mainMethod};	// main() is implicitly instrumented
+	ConstraintSet startingConstraints;
+
+	for (auto node : (*graph)) {
+		if (CgHelper::isConjunction(node)) {
+
+			CgNodePtrSet currentParents;
+
+			for (auto parentNode : node->getParentNodes()) {
+				currentParents.insert(parentNode);
+			}
+
+			startingParents.insert(currentParents.begin(), currentParents.end());
+			startingConstraints.insert(OptimalNodeBasedConstraint(currentParents));
+		}
+	}
+
+	optimalInstrumentation = startingParents;
+	stateStack.push(OptimalNodeBasedState(startingParents, startingConstraints));
+}
---
+++ src/OptimalNodeBasedEstimatorPhase.h
@@ -0,0 +1,88 @@
+
+#ifndef OPTIMALNODEBASEDESTIMATORPHASE_H_
+#define OPTIMALNODEBASEDESTIMATORPHASE_H_
+
+#include "EstimatorPhase.h"
+#include "CgNode.h"
+#include "CgHelper.h"
+
+#include <stack>
+
+struct OptimalNodeBasedConstraint;
+struct OptimalNodeBasedState;
+
+typedef std::set<OptimalNodeBasedConstraint> ConstraintSet;
+
+
+class OptimalNodeBasedEstimatorPhase : public EstimatorPhase {
+public:
+	OptimalNodeBasedEstimatorPhase();
+	~OptimalNodeBasedEstimatorPhase();
+
+	void modifyGraph(CgNodePtr mainMethod);
+
+protected:
+	void printAdditionalReport();
+
+private:
+	std::stack<OptimalNodeBasedState> stateStack;
+
+	CgNodePtrSet optimalInstrumentation;
+	unsigned long long optimalCosts;
+
+
+	void findStartingState(CgNodePtr mainMethod);
+};
+
+struct OptimalNodeBasedConstraint {
+	size_t size;
+	CgNodePtrSet elements;
+
+	OptimalNodeBasedConstraint(CgNodePtrSet elements) {
+		this->elements = elements;
+		size = elements.size();
+	}
+
+	// XXX RN: unused?
+	bool validAfterExchange(CgNodePtr oldElement, CgNodePtr newElement) {
+		elements.erase(oldElement);
+		elements.insert(newElement);
+
+		return elements.size() == size;
+	}
+
+	bool validAfterExchange(CgNodePtr oldElement, CgNodePtrSet newElements) {
+		size += (newElements.size() - 1);
+
+		elements.erase(oldElement);
+		elements.insert(newElements.begin(), newElements.end());
+
+		return elements.size() == size;
+	}
+};
+
+struct OptimalNodeBasedState {
+	CgNodePtrSet currentInstrumentation;
+	ConstraintSet constraints;
+
+	OptimalNodeBasedState(CgNodePtrSet startingSet, ConstraintSet startingConstraints) {
+		this->currentInstrumentation = startingSet;
+		this->constraints = startingConstraints;
+	}
+
+	bool validAfterExchange(CgNodePtr oldElement, CgNodePtrSet newElements) {
+
+		currentInstrumentation.erase(oldElement);
+		currentInstrumentation.insert(newElements.begin(), newElements.end());
+
+		for (OptimalNodeBasedConstraint constraint : constraints) {
+			if (!constraint.validAfterExchange(oldElement, newElements)) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+
+#endif
