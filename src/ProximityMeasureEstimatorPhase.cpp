fb4871708c3905be85d6a4c6ce12adaf49c1be79
Jan Lehr <jp.lehr@gmail.com>
2015-06-10 14:58:10 +0200

    Added functinality to be able to compute ratios between a functions and its children in terms of runtime.

--- src/ProximityMeasureEstimatorPhase.cpp
+++ src/ProximityMeasureEstimatorPhase.cpp
@@ -4,60 +4,128 @@ ProximityMeasureEstimatorPhase::ProximityMeasureEstimatorPhase(
     std::string filename)
     : EstimatorPhase("proximity-estimator"), filename(filename),
       compareAgainst(CubeCallgraphBuilder::build(filename, 1)) {
-				std::cerr << "Constructed Proximity Estimator" << std::endl;
-			}
+  std::cerr << "Constructed Proximity Estimator" << std::endl;
+}
 
-void
-ProximityMeasureEstimatorPhase::modifyGraph(CgNodePtr mainMethod){
-	std::cerr << "Running estimation" << std::endl;
+void ProximityMeasureEstimatorPhase::modifyGraph(CgNodePtr mainMethod) {
+  std::cerr << "Running estimation" << std::endl;
   long numFuncsFull = graph->size();
   long numFuncsOther = compareAgainst.size();
   std::cout << numFuncsFull / numFuncsOther
             << "\% of the originally recorded functions preserved" << std::endl;
 
   // we get the node ptr from the complete profile
-	std::cout << "Children preserved in profile: " << childrenPreservedMetric(mainMethod) << std::endl;
+  std::cout << "Children preserved in profile: "
+            << childrenPreservedMetric(mainMethod) << std::endl;
+  std::cout << "Runtime of main compared to its children: "
+            << portionOfRuntime(mainMethod) << std::endl;
+  std::cout << "Runtime of functions\n";
+  for (const auto &n : mainMethod->getChildNodes()) {
+    portionOfRuntime(n);
+  }
 }
 
-void ProximityMeasureEstimatorPhase::prepareList(CgNodePtr mainM){
-	worklist.insert(mainM);
-	for(const auto n : mainM->getChildNodes()){
-		if(worklist.find(n) == worklist.end())
-			prepareList(n);
-	}
-}
-
-double
-ProximityMeasureEstimatorPhase::childrenPreservedMetric(CgNodePtr origFunc){
-
-	prepareList(mainMethod);
-	std::cout << "Profile has " << worklist.size() << " nodes" << std::endl;
-
-	double val = 0.0;
-	// for all functions in original CG, find corresponding node in filtered CG
-	// if node is found calculate childrenPreserved(orig, filtered)
-	for(auto &origNode : worklist){
-		for(auto fNode = compareAgainst.begin(); fNode != compareAgainst.end(); ++fNode){
-			
-			CgNodePtr funcNode = *fNode;
-			
-			if(origNode->isSameFunction(funcNode)){
-				double cs = childrenPreserved(origNode, funcNode);
-				val += cs;
-			}
-		}
-	}
-	return val / worklist.size();
+void ProximityMeasureEstimatorPhase::printReport() {
+  std::cout << "==== ProximityMeasure Reporter ====\n";
 }
 
 double
-ProximityMeasureEstimatorPhase::childrenPreserved(CgNodePtr orig, CgNodePtr filtered){
-	if(orig->getChildNodes().size() == 0){
-		if(filtered->getChildNodes().size() > 0){
-			std::cerr << "Warning: No child nodes in original profile but filtered profile." << std::endl;
-		}
-		return 1.0;
-	}
-	return double(filtered->getChildNodes().size()) / orig->getChildNodes().size();
+ProximityMeasureEstimatorPhase::childrenPreservedMetric(CgNodePtr origFunc) {
+
+  prepareList(origFunc);
+
+  double val = 0.0;
+  // for all functions in original CG, find corresponding node in filtered CG
+  // if node is found calculate childrenPreserved(orig, filtered)
+  for (auto &origNode : worklist) {
+    CgNodePtr funcNode = getCorrespondingComparisonNode(origNode);
+
+    if (funcNode == nullptr)
+      continue;
+
+    double cs = childrenPreserved(origNode, funcNode);
+    val += cs;
+  }
+  return val / worklist.size();
+}
+
+// returns the CgNode with the same function as node
+CgNodePtr ProximityMeasureEstimatorPhase::getCorrespondingComparisonNode(
+    const CgNodePtr node) {
+  for (auto fNode = compareAgainst.begin(); fNode != compareAgainst.end();
+       ++fNode) {
+
+    CgNodePtr funcNode = *fNode;
+
+    if (node->isSameFunction(funcNode)) {
+      return funcNode;
+    }
+  }
+  return nullptr;
+}
+
+double ProximityMeasureEstimatorPhase::portionOfRuntime(CgNodePtr node) {
+
+  std::pair<double, double> runtime = getExclusiveAndChildrenRuntime(node);
+
+  CgNodePtr compNode = getCorrespondingComparisonNode(node);
+  if (compNode == nullptr) {
+    return 0.0;
+  }
+
+  std::pair<double, double> runtimeInCompareProfile =
+      getExclusiveAndChildrenRuntime(compNode);
+
+  std::cout.precision(12);
+  std::cout << "====\nRuntime: " << runtime.first
+            << "\nCompared runtime: " << runtimeInCompareProfile.first
+            << "\nRuntime of children: " << runtime.second
+            << "\nRuntime of children: " << runtimeInCompareProfile.second
+            << "\n====" << std::endl;
+  std::cout << "==== Ratio between runtimes ====\nOriginal profile: "
+            << runtime.second / runtime.first << "\nCompared profile: "
+            << runtimeInCompareProfile.second / runtimeInCompareProfile.first
+            << "\n" << std::endl;
+
+  return runtime.first / runtime.second;
+}
+
+std::pair<double, double>
+ProximityMeasureEstimatorPhase::getExclusiveAndChildrenRuntime(CgNodePtr node) {
+  worklist.clear();
+  prepareList(node);
+
+  double runtimeInSeconds = node->getRuntimeInSeconds();
+  // the init value is -runtimeInSeconds, because worklist contains "node"
+  // which we don't want to include in the summation
+  double runtimeSumOfChildrenInSeconds =
+      std::accumulate(worklist.begin(), worklist.end(), -runtimeInSeconds,
+                      [](const double &a, const CgNodePtr &b) {
+                        return a + b->getRuntimeInSeconds();
+                      });
+
+	runtimeInSeconds += runtimeSumOfChildrenInSeconds;
+  return std::make_pair(runtimeInSeconds, runtimeSumOfChildrenInSeconds);
+}
+
+double ProximityMeasureEstimatorPhase::childrenPreserved(CgNodePtr orig,
+                                                         CgNodePtr filtered) {
+  if (orig->getChildNodes().size() == 0) {
+    if (filtered->getChildNodes().size() > 0) {
+      std::cerr << "[Warning]: Should not happen!\n\t>>No child nodes in "
+                   "original profile but filtered profile." << std::endl;
+    }
+    return 1.0;
+  }
+  return double(filtered->getChildNodes().size()) /
+         orig->getChildNodes().size();
+}
+
+void ProximityMeasureEstimatorPhase::prepareList(CgNodePtr mainM) {
+  worklist.insert(mainM);
+  for (const auto n : mainM->getChildNodes()) {
+    if (worklist.find(n) == worklist.end())
+      prepareList(n);
+  }
 }
 
--- src/ProximityMeasureEstimatorPhase.h
+++ src/ProximityMeasureEstimatorPhase.h
@@ -5,21 +5,35 @@
 #include "CubeReader.h"
 #include "EstimatorPhase.h"
 
-
 class ProximityMeasureEstimatorPhase : public EstimatorPhase {
 
 public:
   ProximityMeasureEstimatorPhase(std::string filename);
   virtual void modifyGraph(CgNodePtr mainMethod) override;
-	double childrenPreservedMetric(CgNodePtr origFunc);
+
+  /**
+   * Calculates for each node n in profile starting from origFunc
+   * val = sum_{F}(childrenPreserved(F, G))/#Func
+   * with F being all functions in the subtree starting at origFunc
+   * and G being all corresponding functions in the filtered profile
+   *
+   * \returns a percentage value how many calls were preserved
+   */
+  double childrenPreservedMetric(CgNodePtr origFunc);
+  double portionOfRuntime(CgNodePtr node);
+
+	void printReport() override;
 
 private:
-	double childrenPreserved(CgNodePtr orig, CgNodePtr filtered);
-	void prepareList(CgNodePtr mainM);
+  double childrenPreserved(CgNodePtr orig, CgNodePtr filtered);
+  void prepareList(CgNodePtr mainM);
+  std::pair<double, double> getExclusiveAndChildrenRuntime(CgNodePtr node);
+	CgNodePtr getCorrespondingComparisonNode(const CgNodePtr node);
 
-	std::string filename;
-	CallgraphManager compareAgainst;
-	std::set<CgNodePtr> worklist;
+
+  std::string filename;
+  CallgraphManager compareAgainst;
+  std::set<CgNodePtr> worklist;
 };
 
 #endif
